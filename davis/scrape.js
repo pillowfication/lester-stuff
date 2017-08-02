e26f4fdb5a090d71e891066f87c615c2f56f3e19
Markus Tran <thebluepillow@gmail.com>
2017-08-02 11:57:14 -0700

    js standard

--- davis/scrape.js
+++ davis/scrape.js
@@ -1,123 +1,127 @@
-const fs = require('fs');
-const path = require('path');
-const request = require('request');
-const cheerio = require('cheerio');
+const fs = require('fs')
+const path = require('path')
+const request = require('request')
+const cheerio = require('cheerio')
+const moment = require('moment')
 
-const BASE_URL = 'http://directory.ucdavis.edu/search/directory_results.shtml';
-const OUTPUT = 'data.json';
+const BASE_URL = 'http://directory.ucdavis.edu/search/directory_results.shtml'
+const OUTPUT = path.join(__dirname, 'data', `${moment().format('YYYY-MM-DD_HH-mm')}.json`)
 
-function pad(id) {
-  return ('00000000' + id).slice(-8);
+function pad (id) {
+  return ('00000000' + id).slice(-8)
 }
 
-function getPage(id) {
+function getPage (id) {
   return new Promise((resolve, reject) => {
     request(
-      {url: `${BASE_URL}?id=${pad(id)}`},
+      { url: `${BASE_URL}?id=${pad(id)}` },
       (error, response, body) =>
         error || !body
           ? reject(error || new Error('Could not load page.'))
           : resolve(body)
-    );
-  });
+    )
+  })
 }
 
-function getTable(html) {
+function getTable (html) {
   return new Promise((resolve, reject) => {
     try {
-      const $ = cheerio.load(html);
-      const table = $('#directory_results_wrapper > table').first();
-      if (table.get().length === 0)
-        return resolve(null);
+      const $ = cheerio.load(html)
+      const table = $('#directory_results_wrapper > table').first()
+      if (table.get().length === 0) {
+        return resolve(null)
+      }
 
-      const data = {};
+      const data = {}
       table.find('tr').each((index, row) => {
-        const first = $(row).children().first();
-        data[first.text()] = first.next().text();
-      });
-      resolve(data);
+        const first = $(row).children().first()
+        data[first.text()] = first.next().text()
+      })
+      resolve(data)
     } catch (error) {
-      reject(error);
+      reject(error)
     }
-  });
+  })
 }
 
-function getIds(start, count) {
+function getIds (start, count) {
   return new Promise((resolve, reject) => {
-    const results = [];
-    let finished = 0;
+    const results = []
+    let finished = 0
 
-    for (let index = 0; index < count; ++index)
+    for (let index = 0; index < count; ++index) {
       getPage(start + index)
         .then(getTable)
         .then(data => {
-          results[index] = data;
-          if (++finished === count)
-            resolve(results);
+          results[index] = data
+          if (++finished === count) {
+            resolve(results)
+          }
         })
-        .catch(reject);
-  });
+        .catch(reject)
+    }
+  })
 }
 
-function scrape(start = 0, end = 99999999, chunk = 100, output = OUTPUT) {
-  output = path.join(__dirname, output);
+function scrape (start = 0, end = 99999999, chunk = 100, output = OUTPUT) {
+  output = path.join(__dirname, output)
 
-  const keys = {};
-  function cleanData(id, data) {
-    const clean = {id};
+  const keys = {}
+  function cleanData (id, data) {
+    const clean = { id }
     for (const key in data) {
-      const cleanKey = key.toLowerCase().replace(/[^a-z]+/g, '');
-      (keys[cleanKey] || (keys[cleanKey] = new Set())).add(key);
-      clean[cleanKey] = data[key];
+      const cleanKey = key.toLowerCase().replace(/[^a-z]+/g, '')
+      ;(keys[cleanKey] || (keys[cleanKey] = new Set())).add(key)
+      clean[cleanKey] = data[key]
     }
-    return clean;
+    return clean
   }
 
-  (function _scrape(curr) {
+  ;(function _scrape (curr) {
     if (curr > end) {
-      console.log('All done here!');
-      console.log(keys);
-      return;
+      console.log('All done here!')
+      console.log(keys)
+      return
     }
 
-    let _chunk = Math.max(1, Math.min(chunk, end - curr));
-    console.log(`Getting: ${pad(curr)} - ${pad(curr + _chunk - 1)}`);
+    let _chunk = Math.max(1, Math.min(chunk, end - curr))
+    console.log(`Getting: ${pad(curr)} - ${pad(curr + _chunk - 1)}`)
 
     getIds(curr, _chunk)
       .then(results => {
-        let found = [];
+        let found = []
         for (let index = 0; index < _chunk; ++index) {
-          const data = results[index];
-          if (data)
-            found.push(cleanData(curr + index, data));
+          const data = results[index]
+          if (data) {
+            found.push(cleanData(curr + index, data))
+          }
         }
         console.log(found.length
           ? `Found ids: ${found.map(data => data.id).join(', ')}`
           : `Found ids: (none)`
-        );
+        )
 
         fs.appendFileSync(output,
           found.map(data => JSON.stringify(data) + '\n').join('')
-        );
+        )
 
-        _scrape(curr + _chunk);
+        _scrape(curr + _chunk)
       })
       .catch(error => {
-        console.log(error);
-        _scrape(curr);
-      });
-  })(start);
+        console.log(error)
+        _scrape(curr)
+      })
+  })(start)
 }
 
 if (require.main === module) {
-  const args = process.argv.slice(2);
-  const start = process.argv[2] && +process.argv[2];
-  const end = process.argv[3] && +process.argv[3];
-  const chunk = process.argv[4] && +process.argv[4];
-  const output = process.argv[5];
-  scrape(start, end, chunk, output);
+  const start = process.argv[2] && +process.argv[2]
+  const end = process.argv[3] && +process.argv[3]
+  const chunk = process.argv[4] && +process.argv[4]
+  const output = process.argv[5]
+  scrape(start, end, chunk, output)
 }
 
 module.exports = {
   getPage, getTable, getIds, scrape
-};
+}
--- davis/toCsv.js
+++ davis/toCsv.js
@@ -1,45 +1,49 @@
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
-const csvStringify = require('csv-stringify');
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+const csvStringify = require('csv-stringify')
 
-const INPUT = 'data.json';
-const OUTPUT = 'data.csv';
-
-function toCsv(input = INPUT, output = OUTPUT) {
-  input = path.join(__dirname, input);
-  output = path.join(__dirname, output);
+function toCsv (input, output) {
+  if (!input) {
+    throw new Error('No input provided')
+  }
+  input = path.join(__dirname, input)
+  const inputParse = path.parse(input)
+  output = output
+    ? path.join(__dirname, output)
+    : path.join(inputParse.dir, `${inputParse.name}.csv`)
 
   const lineReader = readline.createInterface({
     input: fs.createReadStream(input)
-  });
+  })
 
-  const columns = {id: 'id'};
-  const csv = [];
+  const columns = { id: 'id' }
+  const csv = []
 
   lineReader.on('line', line => {
-    const obj = JSON.parse(line);
-    for (const key in obj)
-      !columns[key] && (columns[key] = key);
-    csv.push(obj);
-  });
+    const obj = JSON.parse(line)
+    for (const key in obj) {
+      !columns[key] && (columns[key] = key)
+    }
+    csv.push(obj)
+  })
 
   lineReader.on('close', () => {
-    csvStringify(csv, {header: true, columns}, (error, csv) => {
-      if (error)
-        throw error;
-      fs.writeFileSync(output, csv);
-    });
-  });
+    csvStringify(csv, { header: true, columns }, (error, csv) => {
+      if (error) {
+        throw error
+      }
+      fs.writeFileSync(output, csv)
+    })
+  })
 }
 
 if (require.main === module) {
-  const args = process.argv.slice(2);
-  const input = process.argv[2];
-  const output = process.argv[3];
-  toCsv(input, output);
+  const input = process.argv[2]
+  const output = process.argv[3]
+  toCsv(input, output)
 }
 
 module.exports = {
   toCsv
-};
+}
