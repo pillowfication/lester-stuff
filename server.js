cb924d40a55beaf5d1e56ffa8d5e8d8475b5a5db
Markus Tran <thebluepillow@gmail.com>
2017-11-09 18:02:01 -0800

    also serve football

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const serveIndex = require('serve-index')
+
+const app = express()
+const port = +process.argv[2] || process.env.LESTER_SCRAPE_PORT || 80
+
+app.use('/davis', express.static(path.join(__dirname, 'davis', 'data')))
+app.use('/davis', serveIndex(path.join(__dirname, 'davis', 'data'), { view: 'details' }))
+app.use('/football', express.static(path.join(__dirname, 'football', 'data')))
+app.use('/football', serveIndex(path.join(__dirname, 'football', 'data'), { view: 'details' }))
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}`)
+})
