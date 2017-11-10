12e5bdf7a4906692543db8f9c551675e5d2e56c5
Markus Tran <thebluepillow@gmail.com>
2017-11-09 17:12:51 -0800

    clean football

--- football/index.js
+++ football/index.js
@@ -1,89 +1,92 @@
-const jsonfile = require('jsonfile');
-const async = require('async');
-const request = require('request');
-const cheerio = require('cheerio');
+const jsonfile = require('jsonfile')
+const async = require('async')
+const request = require('request')
+const cheerio = require('cheerio')
 
-const baseUrl = 'http://collegepollarchive.com/football/ap/seasons.cfm?appollid=';
+const BASE_URL = 'http://collegepollarchive.com/football/ap/seasons.cfm?appollid='
 
-function requestId(id, cb) {
-  request.get(`${baseUrl}${id}`, (err, res, html) => {
-    if (err)
-      return cb(err);
+function requestId (id, callback) {
+  request.get(`${BASE_URL}${id}`, (error, _, html) => {
+    if (error) {
+      return callback(error)
+    }
 
-    const $ = cheerio.load(html, {normalizeWhitespace: true});
+    const $ = cheerio.load(html, { normalizeWhitespace: true })
 
     const heading = $('h2').filter((index, h2) => {
-      return /Football Poll$/.test($(h2).text());
-    });
+      return /Football Poll$/.test($(h2).text())
+    })
 
-    const table = heading.nextAll('table').eq(0);
+    const table = heading.nextAll('table').eq(0)
 
     if (!table.length) {
-      cb(new Error('Could not find the table of data.'));
+      callback(new Error('Could not find the table of data.'))
     } else {
-      cb(null, {
+      callback(null, {
         heading: heading.text(),
         table: parseTable($, table)
-      });
+      })
     }
-  });
+  })
 }
 
-function parseTable($, table, cb) {
-  const thData = [];
+function parseTable ($, table) {
+  const thData = []
   $('tr', table).eq(0).children('th').each((index, th) => {
     thData.push({
       text: $(th).text(),
       span: +$(th).attr('colspan') || 1
-    });
-  });
+    })
+  })
 
-  const cols = thData.reduce((sum, th) => sum + th.span, 0);
-  const data = [];
+  const cols = thData.reduce((sum, th) => sum + th.span, 0)
+  const data = []
 
   $('tr', table).each((index, tr) => {
-    const tds = $('td', tr);
-    if (tds.length !== cols)
-      return;
+    const tds = $('td', tr)
+    if (tds.length !== cols) {
+      return
+    }
 
-    const datum = {};
-    let td = 0;
+    const datum = {}
+    let td = 0
     thData.forEach(th => {
       if (th.span === 1) {
-        datum[th.text] = tds.eq(td).text().trim();
-        ++td;
+        datum[th.text] = tds.eq(td).text().trim()
+        ++td
       } else {
-        datum[th.text] = [];
-        for (let inc = 0; inc < th.span; ++inc)
-          datum[th.text].push(tds.eq(td + inc).text().trim());
-        td += th.span;
+        datum[th.text] = []
+        for (let inc = 0; inc < th.span; ++inc) {
+          datum[th.text].push(tds.eq(td + inc).text().trim())
+        }
+        td += th.span
       }
-    });
-    data.push(datum);
-  });
+    })
+    data.push(datum)
+  })
 
-  return data;
+  return data
 }
 
-const [start, end] = [671, 1103];
+const [ start, end ] = [ 671, 1103 ]
 async.timesSeries(end - start + 1, (inc, next) => {
-  const id = start + inc;
-  console.log(`Fetching id: ${id}...`);
-  requestId(id, next);
-}, (err, results) => {
-  if (err) {
-    console.log('SOMETHING BAD HAPPENED');
-    console.log(err);
+  const id = start + inc
+  console.log(`Fetching id: ${id}...`)
+  requestId(id, next)
+}, (error, results) => {
+  if (error) {
+    console.log('SOMETHING BAD HAPPENED')
+    console.log(error)
   } else {
-    const data = {};
+    const data = {}
     results.forEach((result, index) => {
-      data[start + index] = result;
-    });
+      data[start + index] = result
+    })
     try {
-      jsonfile.writeFileSync('data.json', data, {spaces: 2});
-    } catch (error) {
-      console.log('Error creating `data.json`. Please try again.');
-      console.log(error);
+      jsonfile.writeFileSync('data.json', data, { spaces: 2 })
+    } catch (_error) {
+      console.log('Error creating `data.json`. Please try again.')
+      console.log(_error)
     }
   }
-});
+})
