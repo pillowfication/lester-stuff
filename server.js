e203a05eb6653ce8d2c2880bdb00739f3005036e
Markus Tran <thebluepillow@gmail.com>
2017-12-11 19:11:28 -0800

    package-lock

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const serveIndex = require('serve-index')
 
 const app = express()
-const port = +process.argv[2] || process.env.LESTER_SCRAPE_PORT || 80
+const port = process.argv[2] || process.env.LESTER_SCRAPE_PORT || 80
 
 app.use('/davis', express.static(path.join(__dirname, 'davis', 'data')))
 app.use('/davis', serveIndex(path.join(__dirname, 'davis', 'data'), { view: 'details' }))
@@ -11,5 +11,5 @@ app.use('/football', express.static(path.join(__dirname, 'football', 'data')))
 app.use('/football', serveIndex(path.join(__dirname, 'football', 'data'), { view: 'details' }))
 
 app.listen(port, () => {
-  console.log(`App started on port ${port}`)
+  console.log(`lester-scrape started on port ${port}`)
 })
