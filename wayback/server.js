a6f69b529a63efe3b54702b02afa36c4e8aff923
Markus Tran <thebluepillow@gmail.com>
2020-07-14 11:29:29 -0700

    asdf

--- wayback/server.js
+++ wayback/server.js
@@ -2,11 +2,24 @@ const path = require('path')
 const express = require('express')
 const serveIndex = require('serve-index')
 
-const ARCHIVE_PATH = path.resolve(__dirname, './archive')
+const ARCHIVE_PATH = 'F:/wayback/archive'
+const htmllikeExt = /^((\.html?|\.php|\.aspx?|\.jsp)(\?.*)?)?$/i
 
 const app = express()
 
-app.use('/archive', express.static(ARCHIVE_PATH))
+app.use((req, res, next) => {
+  console.log(req.url)
+  next()
+})
+
+app.use('/archive', express.static(ARCHIVE_PATH, {
+  setHeaders: (res, _path) => {
+    const extension = path.extname(_path)
+    if (htmllikeExt.test(extension)) {
+      res.setHeader('Content-Type', 'text/html')
+    }
+  }
+}))
 app.use('/archive', serveIndex(ARCHIVE_PATH, {
   hidden: true,
   icons: true,
